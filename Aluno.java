bdd68e7c0cf8d2e78b7f6f42844130c378020bc1
TiagoDesc <nobretiago96@gmail.com>
2024-04-11 15:44:05 -0300

    new commit

--- Aluno.java
+++ Aluno.java
@@ -1,7 +1,31 @@
 public class Aluno {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    private String name;
+
+    public Aluno() {
+
+    }
+
+    public Aluno(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outro = (Aluno) obj;
+        return outro.getName().equals(this.name);
+    }
+
+    public String toString() {
+        return name;
     }
 
 }
