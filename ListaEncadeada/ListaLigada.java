d0db7d85c5681cb32110d8b5d67495a02778edb1
TiagoDesc <nobretiago96@gmail.com>
2024-04-22 19:01:19 -0300

    remove

--- ListaEncadeada/ListaLigada.java
+++ ListaEncadeada/ListaLigada.java
@@ -38,17 +38,6 @@ public class ListaLigada {
         return atual;
     }
 
-    public Celula pega1(int posicao) {
-        Celula atual = this.primeira;
-        for (int i = 0; i < posicao; i++) {
-            if (atual.getProximo() != null) {
-                atual = atual.getProximo();
-            }
-        }
-
-        return atual;
-    }
-
     public Celula pega2(int posicao) {
         Celula atual = this.primeira;
         for (int i = 0; i < posicao; i++) {
